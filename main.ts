5fe46ca996583e920ce08f654612075ccd0c1963
rtator <66912559+rtator@users.noreply.github.com>
2020-07-09 09:10:59 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,350 @@
+namespace SpriteKind {
+    export const bug = SpriteKind.create()
+    export const fish = SpriteKind.create()
+    export const ameoba = SpriteKind.create()
+}
+namespace myTiles {
+    //% blockIdentity=images._tile
+    export const tile0 = img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`
+    //% blockIdentity=images._tile
+    export const tile1 = img`
+. e e e e e . e e . e e e e e . 
+. e e . . e e e e e e . . e e . 
+7 7 7 . . . e e e e . . . 7 7 7 
+7 7 7 . . . e e e e . . . 7 7 7 
+7 7 7 . . . e e e e . . . 7 7 7 
+. . . . . . e e e e . . . . . . 
+. . . . . . e e e e . . . . . . 
+. . . . . . e e e e . . . . . . 
+. . . . . . e e e e . . . . . . 
+. . . . . . e e e e . . . . . . 
+. . . . . . e e e e . . . . . . 
+. . . . . . e e e e . . . . . . 
+. . . . . . e e e e . . . . . . 
+. . . . . . e e e e . . . . . . 
+. . . . . . e e e e . . . . . . 
+. . . . . . e e e e . . . . . . 
+`
+    //% blockIdentity=images._tile
+    export const tile3 = img`
+. . . . . . . e e . e . . . . . 
+. . . . . . e e e e e . . . . . 
+. . . . . . e e e . . . . . . . 
+. . . . . . e e e . . . . . . . 
+. . . . . . e e e . . . . . . . 
+. . . . . . e e e . . . . . . . 
+. . . . . . e e e . . . . . . . 
+. . . . . . e e e . . . . . . . 
+. . . . . . e e e . . . . . . . 
+. . . . . . e e e . . . . . . . 
+. . . . . . e e e . . . . . . . 
+. . . . . . e e e . . . . . . . 
+. . . . . . e e e . . . . . . . 
+. . . . . e e e e e . . . . . . 
+. . . . e e e e e e e . . . . . 
+. . . . e e e e e e e . . . . . 
+`
+    //% blockIdentity=images._tile
+    export const tile5 = img`
+. . . e e e e e e e e e e . . . 
+. . . e e e e e e e e e e . . . 
+. . . e e e e e e e e e e . . . 
+. . . e e e e e e e e e e . . . 
+. . . . e e e e e e e e . . . . 
+. . . . . e e e e e e . . . . . 
+. . . . . e e e e e e . . . . . 
+. . . . . e e e e e e . . . . . 
+. . . . . e e e e e e . . . . . 
+. . . . . e e e e e e . . . . . 
+. . . . . e e e e e e . . . . . 
+. . . . e e e e e e e e . . . . 
+. . . e e e e e e e e e e . . . 
+. . . e e e e e e e e e e . . . 
+. . . e e e e e e e e e e . . . 
+. . . e e e e e e e e e e . . . 
+`
+    //% blockIdentity=images._tile
+    export const tile6 = img`
+d b d d d d d d d d d d d d d d 
+d d d d d b d d d d d d d b d d 
+d d d d d d d d d d 1 d d d d d 
+d b d d d d d d d d d d d d d d 
+d d d 1 d d d d d d d d d d d d 
+d d d d d d d d d d d d d d b d 
+d d d d d d d d d d d d d d d d 
+d d d d d d d b d d d d d d d d 
+d d d d d d d d d d d 1 d d d d 
+d b d d d d d d d d d d d d d d 
+d d d d d d d d d d d d d d d d 
+d d d d d d d 1 d d d d d d d d 
+d 1 d d d d d d d d d d d d b d 
+d d d d d d d d d d b d d d d d 
+d d b d d d d d d d d d d d d d 
+d d d d d d b d d d d d d d d 1 
+`
+    //% blockIdentity=images._tile
+    export const tile7 = img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . 1 1 1 
+. . . . . . . . . . . . 1 1 1 1 
+. . . . . . . . . . . 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e . . . . . . . . . . . . e e 
+e e . . . . . . . . . . . . e e 
+`
+    //% blockIdentity=images._tile
+    export const tile8 = img`
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+`
+}
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.setImage(img`
+. . . . . 2 2 2 2 2 . . . 
+. . . 2 2 2 2 2 2 2 2 2 . 
+. . 2 2 2 2 2 2 2 2 2 2 . 
+. . 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 
+. f f f e e f f f f f f f 
+. . f f e e f b f e e f f 
+. . . f 4 4 f 1 e 4 e f . 
+. . . f 4 4 4 4 e f f f . 
+. . . f f e e e e e f . . 
+. . . f 7 7 7 e 4 4 e . . 
+. . . f 7 7 7 e 4 4 e . . 
+. . . f 6 6 6 f e e f . . 
+. . . . f f f f f f . . . 
+. . . . . . f f f . . . . 
+`)
+    is_facing_left = true
+})
+function place_bug () {
+    if (Math.percentChance(50)) {
+        tiles.placeOnRandomTile(mySprite2, sprites.castle.shrub)
+    } else if (Math.percentChance(50)) {
+        tiles.placeOnRandomTile(mySprite2, myTiles.tile1)
+    } else if (Math.percentChance(50)) {
+        tiles.placeOnRandomTile(mySprite2, myTiles.tile3)
+    } else {
+        tiles.placeOnRandomTile(mySprite2, myTiles.tile5)
+    }
+}
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (is_facing_left) {
+        projectile = sprites.createProjectileFromSprite(img`
+. 1 1 . 
+1 1 1 1 
+1 9 1 1 
+1 1 9 1 
+1 1 1 1 
+1 9 1 1 
+1 1 9 1 
+. 1 1 . 
+`, mySprite, -50, 0)
+    } else if (!(is_facing_left)) {
+        projectile = sprites.createProjectileFromSprite(img`
+. 1 1 . 
+1 1 1 1 
+1 9 1 1 
+1 1 9 1 
+1 1 1 1 
+1 9 1 1 
+1 1 9 1 
+. 1 1 . 
+`, mySprite, 50, 0)
+    }
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.setImage(img`
+. . . 2 2 2 2 2 . . . . . 
+. 2 2 2 2 2 2 2 2 2 . . . 
+. 2 2 2 2 2 2 2 2 2 2 . . 
+2 2 2 2 2 2 2 2 2 2 2 . . 
+2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 
+f f f f f f f e e f f . . 
+f f e e f b f e e f f . . 
+. f e 4 e 1 f 4 4 f . . . 
+. f f f e 4 4 4 4 f . . . 
+. . f e e e e e f f . . . 
+. . e 4 4 e 7 7 7 f . . . 
+. . e 4 4 e 7 7 7 f . . . 
+. . f e e f 6 6 6 f . . . 
+. . . f f f f f f . . . . 
+. . . . f f f . . . . . . 
+`)
+    is_facing_left = false
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.bug, function (sprite, otherSprite) {
+    sprite.destroy()
+    otherSprite.destroy()
+})
+sprites.onDestroyed(SpriteKind.ameoba, function (sprite) {
+    game.showLongText("" + name + ", you caught an amoeba (2 point)!", DialogLayout.Bottom)
+    info.changeScoreBy(2)
+    create_and_place_amoeba_2()
+})
+function create_and_place_fish () {
+    for (let index = 0; index < 5; index++) {
+        mySprite3 = sprites.create(img`
+7 7 7 
+7 7 7 
+7 7 7 
+`, SpriteKind.ameoba)
+        tiles.placeOnRandomTile(mySprite3, myTiles.tile8)
+    }
+}
+function create_and_place_amoeba_2 () {
+    mySprite3 = sprites.create(img`
+7 7 7 
+7 7 7 
+7 7 7 
+`, SpriteKind.ameoba)
+    tiles.placeOnRandomTile(mySprite3, myTiles.tile8)
+}
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (mySprite.tileKindAt(TileDirection.Bottom, myTiles.tile8) || mySprite.vy == 0) {
+        mySprite.vy = -90
+    }
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.ameoba, function (sprite, otherSprite) {
+    sprite.destroy()
+    otherSprite.destroy()
+})
+function bug_create () {
+    mySprite2 = sprites.create(img`
+f f f 
+f f f 
+f f f 
+`, SpriteKind.bug)
+    is_bug_created = true
+}
+sprites.onDestroyed(SpriteKind.bug, function (sprite) {
+    if (Math.percentChance(40)) {
+        game.showLongText("" + name + ", you caught a beetle (3 points)!", DialogLayout.Bottom)
+        info.changeScoreBy(3)
+    } else if (Math.percentChance(60)) {
+        game.showLongText("" + name + ", you caught a cicada (1 point!", DialogLayout.Bottom)
+        info.changeScoreBy(1)
+    } else if (Math.percentChance(50)) {
+        game.showLongText("" + name + ", you caught a dragon fly (2 point)!", DialogLayout.Bottom)
+        info.changeScoreBy(2)
+    } else {
+        game.showLongText("" + name + ", you caught a decoy (0 point).", DialogLayout.Bottom)
+    }
+    bug_create()
+    place_bug()
+})
+let mySprite3: Sprite = null
+let projectile: Sprite = null
+let mySprite2: Sprite = null
+let name = ""
+let is_facing_left = false
+let is_bug_created = false
+let mySprite: Sprite = null
+mySprite = sprites.create(img`
+. . . . . 2 2 2 2 2 . . . 
+. . . 2 2 2 2 2 2 2 2 2 . 
+. . 2 2 2 2 2 2 2 2 2 2 . 
+. . 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 
+. f f f e e f f f f f f f 
+. . f f e e f b f e e f f 
+. . . f 4 4 f 1 e 4 e f . 
+. . . f 4 4 4 4 e f f f . 
+. . . f f e e e e e f . . 
+. . . f 7 7 7 e 4 4 e . . 
+. . . f 7 7 7 e 4 4 e . . 
+. . . f 6 6 6 f e e f . . 
+. . . . f f f f f f . . . 
+. . . . . . f f f . . . . 
+`, SpriteKind.Player)
+controller.moveSprite(mySprite, 100, 0)
+tiles.setTilemap(tiles.createTilemap(
+            hex`5a0014000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000f0f0f0f0f0f0f0f0f0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000f000000000000000f0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000f000000000000000f00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000002000000000300000000000002000003000a0000000f000000000000000f000002020202020000000003000303030303000300030a00000a000000000a00000200000a00020000000000000000000000000000000000000000000700040000080000000a000007000008000c000000000000001000000000000007070707070004000008000808080808000800080c00000c000004000c00000700000c0007000000000000000000000000000000000000000000060506050605060505050605050505060505050605050505050605050505050505050505050605050505050505050506050505050505060505050505050605050506050505050d0d0d0d0d0d0d0d0d0d0d0d0d11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111121212121212121212121212121212121212121212121212121212121212121212121212121212121212121212121212121212121212121212121212121212121212121212121212121212121212121212121212121212121212`,
+            img`
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . 2 2 2 2 2 2 2 2 2 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . 2 . . . . . . . 2 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . 2 . . . . . . . 2 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+2 . . . . 2 . . . . . . 2 . . 2 . 2 . . . 2 . . . . . . . 2 . . 2 2 2 2 2 . . . . 2 . 2 2 2 2 2 . 2 . 2 2 . . 2 . . . . 2 . . 2 . . 2 . 2 . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . 2 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+`,
+            [myTiles.tile0,sprites.castle.tilePath5,sprites.castle.saplingPine,sprites.castle.saplingOak,sprites.castle.shrub,sprites.castle.tileGrass1,sprites.castle.tileGrass3,myTiles.tile1,myTiles.tile3,sprites.castle.rock0,sprites.builtin.forestTiles0,sprites.dungeon.darkGroundWest,myTiles.tile5,myTiles.tile6,sprites.vehicle.roadTurn2,sprites.builtin.brick,myTiles.tile7,myTiles.tile8,sprites.builtin.coral5,sprites.builtin.coral1,sprites.builtin.oceanDepths0],
+            TileScale.Sixteen
+        ))
+scene.setBackgroundColor(9)
+is_bug_created = false
+mySprite.x += -50
+tiles.placeOnRandomTile(mySprite, myTiles.tile7)
+scene.cameraFollowSprite(mySprite)
+is_facing_left = true
+name = game.askForString("What is your name", 12)
+bug_create()
+place_bug()
+create_and_place_fish()
+game.onUpdate(function () {
+    if (mySprite.tileKindAt(TileDirection.Center, myTiles.tile8)) {
+        mySprite.ay = 30
+        controller.moveSprite(mySprite, 50, 10)
+    } else {
+        mySprite.ay = 200
+        controller.moveSprite(mySprite, 100, 0)
+    }
+})
